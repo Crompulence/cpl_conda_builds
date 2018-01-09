01bb9328b6a1492d7383560a38c2a862781133ef
Eduardo Ramos <eduradical951@gmail.com>
2018-01-09 09:36:16 +0000

    All packages working with config.rc.

--- openfoam3.0.1/build.sh
+++ openfoam3.0.1/build.sh
@@ -1,19 +1,20 @@
 FOAM_VERSION=3.0.1
 FOAM_DIR_NAME=OpenFOAM-$FOAM_VERSION
+FOAM_SRC_DIR=$SRC_DIR/$FOAM_DIR_NAME
 CONDA_PYTHON=$(conda info --root)/bin/python
 BUILD_MODE="download"
 
 
 if [ $BUILD_MODE == "download" ]; then
 	${CONDA_PYTHON} ${RECIPE_DIR}/download-extra-sources.py
-	cp ${RECIPE_DIR}/prefs.sh $SRC_DIR/etc
-	cp ${RECIPE_DIR}/prefs.csh $SRC_DIR/etc
-	export FOAM_INST_DIR=$(dirname $SRC_DIR)
-else
-	export FOAM_INST_DIR=$SRC_DIR
+	cp ${RECIPE_DIR}/prefs.sh $FOAM_SRC_DIR/etc
+	cp ${RECIPE_DIR}/prefs.csh $FOAM_SRC_DIR/etc
+	# Move the third party directory as openfoam expects it in the same folder
+        mv $SRC_DIR/../ThirdParty-$FOAM_VERSION $SRC_DIR	
 fi
 
-foamDotFile=$FOAM_INST_DIR/$FOAM_DIR_NAME/etc/bashrc
+export FOAM_INST_DIR=$SRC_DIR
+foamDotFile=$FOAM_SRC_DIR/etc/bashrc
 echo $foamDotFile
 if [ -f $foamDotFile ]; then
     source $foamDotFile
@@ -28,11 +29,11 @@ export LIBRARY_PATH=$LIBRARY_PATH:$PREFIX/lib
 
 # Only build if the sources are downloaded
 if [ $BUILD_MODE == "download" ]; then
-	cd $SRC_DIR/../ThirdParty-3.0.1
+	cd $SRC_DIR/ThirdParty-$FOAM_VERSION
 	cp $RECIPE_DIR/3rdParty3.0.1makeCGAL.patch .
 	patch < 3rdParty3.0.1makeCGAL.patch
 	./Allwmake -j
-	cd $SRC_DIR
+	cd $FOAM_SRC_DIR
 	./Allwmake -j
 fi
 
@@ -42,4 +43,4 @@ cp $FOAM_EXT_LIBBIN/mpi-system/* $PREFIX/lib
 
 # Copy the whole directory structure to $PREFIX/opt:
 mkdir $PREFIX/opt
-cp -R $FOAM_INST_DIR/$FOAM_DIR_NAME $PREFIX/opt
+cp -R $FOAM_SRC_DIR $PREFIX/opt
